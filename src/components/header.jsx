40048aabed0f1957b217328e8048657108a2147a
oshadawick <doshada32@gmail.com>
2023-09-23 06:26:07 +0530

    add little description

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,10 @@
 export function Header() {
   return (
-    <header className="mt-8 text-center justify-center">
+    <header className="mt-8 text-center justify-center pb-2">
       <h1 className="text-3xl md:text-accent text-base-content font-bold">
         SongEmojis
       </h1>
+      <span className="text-blue-200">can you guess the song by emojis</span>
     </header>
   );
 }
